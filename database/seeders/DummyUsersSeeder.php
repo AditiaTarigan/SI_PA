305a989852735c8e8754cf280f7a5ccbfc188727
AditiaTarigan <aditiatarigan333@gmail.com>
2025-03-10 14:03:09 +0700

    update SI_PA first

---
+++ database/seeders/DummyUsersSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DummyUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userData = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'password' => bcrypt('password2024'),
+            ],
+        ];
+
+        foreach ($userData as $key => $value){
+            User::create($value);
+        }
+
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="png" href="{{ asset('foto/del.png') }}">
+    <title>SIPA</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+<body>
+    <div class="main-container">
+        <div class="form-wrapper">
+            <img src="{{ asset('foto/del.png') }}" alt="del">
+            <h1>Institut Teknologi Del</h1>
+            <form action="" method="POST">
+                @csrf
+                <div class="field">
+                    <input class="inp" type="username" value="" id="username" required autocomplete="off">
+                    <label class="label" for="username">Username</label>
+                    <span class="bi bi-person"></span>
+                </div>
+                <div class="field">
+                    <input class="inp" type="password" id="password" required>
+                    <label class="label" for="password">Password</label>
+                    <span class="toggle-pass bi bi-eye"></span>
+                </div>
+                <div class="action">
+                    <label for="save-info">
+                        <input type="checkbox" id="save-info">
+                        Stay signed in</label>
+                </div>
+                <input type="submit" value="Login" id="login-btn">
+            </form>
+        </div>
+        <div class="bg"></div>
+   </div>
+</body>
+<script>
+    const input = document.querySelector('#password');
+const icon = document.querySelector('.toggle-pass');
+icon.addEventListener('click', () => {
+    icon.classList.toggle('bi-eye-slash');
+    icon.classList.toggle('bi-eye');
+    input.type = (input.type === 'password') ? 'text' : 'password';
+});
+</script>
+</html>
